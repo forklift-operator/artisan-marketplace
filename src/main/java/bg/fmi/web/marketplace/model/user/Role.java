028ff138c6ddedb62fe6a04893917a2503d410c6
Stoyan Yordanov <137334856+forklift-operator@users.noreply.github.com>
2026-04-28 00:14:36 +0300

    added user entity and roles

---
+++ src/main/java/bg/fmi/web/marketplace/model/user/Role.java
@@ -0,0 +1,7 @@
+package bg.fmi.web.marketplace.model.user;
+
+public enum Role {
+    ADMIN,
+    USER,
+    VENDOR
+}
---
+++ src/main/java/bg/fmi/web/marketplace/model/user/User.java
@@ -0,0 +1,31 @@
+package bg.fmi.web.marketplace.model.user;
+
+import bg.fmi.web.marketplace.model.product.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+    @OneToMany(mappedBy = "user")
+    List<Product> products;
+}
